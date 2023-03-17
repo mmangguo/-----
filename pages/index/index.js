6a59933d3dfd84cfc056885dcd15650aedf549f1
Li Fei <15038575@qq.com>
2023-03-18 01:06:49 +0800

    2.0.0大改版
    重构代码逻辑，更新UI，实现自定义菜单

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,49 +1,86 @@
-// index.js
-var app=getApp()
-var Paths=['老杨禅面','烧鸭饭','多一点烤肉饭','杀猪粉','锅锅相伴','黄焖鸡'];
-var Index=0;  //初始值
 Page({
-  //页面初始数据
   data:{
-    Path:Paths[0],
-    title:'吃什么捏',
-    isRunning:false,
-    userInfo:{},
+    showBtn:false,
+    changeBtn:false,
+    foodName:'',
+    timer:null,
+    randomTextList:[],
+    visible:false,
+    textAreaValueList:['老杨禅面','花雕醉鸡','隆兴烧鹅饭','多一点烤肉饭','膳当家黄焖鸡','格子碗','餐仓煲煲仔饭','挑香小面','杀猪粉','老麻抄手','铜炉鸡锅','牛肉汤','谢宝林烤肉饭','鸡公煲','辣椒炒肉粉','锅锅相伴','乌鸡面','叔叔家咖喱','秦味面馆','麻辣烫','牛油拌饭','黄焖鸡米饭'],
+    textAreaValue:'',
   },
 
-  binViewTap:function(){
-    wx.navigateTo({
-      url: '../logs/logs',
-    })
-  },
-  change:function(e){
-    Index++;
-    if (Index>6){  //重置序号
-      Index=0;
-    }
+  onLoad(){
+    const foodNameStr=wx.getStorageSync('foodNameStr');
     this.setData({
-      Path:Paths[Index]
+      textAreaValue:foodNameStr||this.data.textAreaValueList.join('，'),
+      textAreaValueList:foodNameStr?foodNameStr.split('，'):this.data.textAreaValueList
+    });
+  },
+
+  start(){
+    const timer=setInterval(()=>{
+      const list=this.data.textAreaValueList;
+      const r=Math.ceil(Math.random()*list.length);
+      const foodName=list[r-1];
+      let randomTextList=[];
+      randomTextList.push({opacity:foodName});
+      this.setData({
+        foodName,
+        randomTextList,
+      })
+    },100);
+    this.setData({
+      showBtn:true,
+      changeBtn:false,
+      timer
+    });
+  },
+
+  stop(){
+    clearInterval(this.data.timer);
+    this.setData({
+      showBtn:false,
+      changeBtn:true,
+      timer:'',
+    });
+  },
+
+  //自定义饭店
+  onOK(){
+    clearInterval(this.data.timer);
+    wx.setStorageSync('foodNameStr', this.data.textAreaValue);
+    this.setData({
+      showBtn:false,
+      changeBtn:false,
+      timer:'',
+      foodName:'',
+      visible:!this.data.visible,
+      textAreaValue:this.data.textAreaValue,
+      textAreaValueList:this.data.textAreaValue.split('，')
+    });
+  },
+
+  onModal(){
+    this.setData({
+      visible:!this.data.visible,
+      textAreaValue:this.data.textAreaValueList.join('，')
     })
   },
-  guess:function(e){
-    let isRunning=this.data.isRunning
-    if(!isRunning){
-      this.setData({
-        title:'就吃它啦',
-        isRunning:true
-      });
-      this.timer=setInterval((function(){
-        this.change()
-      }).bind(this),50);  //名字滚动的频率
-    }
-    else{ 
-      this.setData(
-        {
-          title:'吃什么捏',
-          isRunning:false
-        }
-      );
-      this.timer&&clearInterval(this.timer);
-    }
-  }
+
+  //退出
+  onHide(){
+    clearInterval(this.data.timer);
+    this.setData({
+      showBtn:false,
+      changeBtn:false,
+      foodName:'',
+      timer:null,
+      visible:false,
+      textAreaValue:'',
+    })
+  },
+
+  //设置分享
+  onShareAppMessage(){}
 })
